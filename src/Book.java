869dc3813d2e7a57ee8ca41224f6180332d37166
Kairav <kairavdutta@gmail.com>
2026-05-22 00:05:48 +0530

    Added library class and shifted some stuff there to increase modularity

--- src/Book.java
+++ src/Book.java
@@ -1,13 +1,9 @@
-import java.io.File;
 import java.io.FileWriter;
-import java.util.Scanner;   
-
 public class Book{
     public String name;
     public String author;
     private int availableCopies; // default
-    private static final int MAX_BOOKS = 1000000;
-    private static final String FILE_NAME = "books.txt";
+
     /**
      * Note: the books are stored in books.txt in this format:
      * bookname, book-author, availablecopies
@@ -33,41 +29,9 @@ public class Book{
             System.out.println("The book has been issued successfully.");
         }
     }
-    private static Book parseLine(String line){
-        int i = 0;
-        String name = "";
-        String author = "";
-        String copies = "";
-        while (i < line.length() && line.charAt(i) != ',')
-            name += line.charAt(i++);
-        i++;
-        while (i < line.length() && line.charAt(i) != ',')
-            author += line.charAt(i++);
-        i++;
-        while (i < line.length() && line.charAt(i) != ',')
-            copies += line.charAt(i++);
-        if (copies.isBlank())
-            return new Book(name, author);
-        return new Book(name, author, (int)Integer.parseInt(copies));
-    }
-    private static Book[] parseLibrary(){
-        Book[] books = new Book[MAX_BOOKS];
-        int i = 0;
-        File file = new File(FILE_NAME);
-        if (file.exists()){
-            try (Scanner in = new Scanner(file)){
-                while (in.hasNextLine())
-                    books[i++] = parseLine(in.nextLine());
-            }catch(Exception e){
-                System.out.println("An exception occured: " + e);
-            }
-            return books;
-        }else{
-            throw new RuntimeException("The file: does not exist");
-        }
-    }
+
     public static Book partOfLibrary(String name, String author){
-        Book[] books = parseLibrary();
+        Book[] books = Library.parseLibrary();
         for (var book: books){
             if (book.name.equals(name) && book.author.equals(author))
                 return book;
@@ -80,7 +44,7 @@ public class Book{
             book.availableCopies += availableCopies;
             // TODO: make updater function
         else{
-            try(FileWriter writer = new FileWriter(FILE_NAME, true)){
+            try(FileWriter writer = new FileWriter(Library.getFileName(), true)){
                 writer.write(name + "," + author + "," + availableCopies);
             }catch(Exception e){
                 throw new RuntimeException("An exception occured: " + e);
---
+++ src/Library.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.util.Scanner;   
+public class Library{
+
+    private static final int MAX_BOOKS = 1000000;
+    private static final String FILE_NAME = "books.txt";
+
+    public static String getFileName(){
+        return FILE_NAME; 
+    }
+
+    private static Book parseLine(String line){
+        int i = 0;
+        String name = "";
+        String author = "";
+        String copies = "";
+        while (i < line.length() && line.charAt(i) != ',')
+            name += line.charAt(i++);
+        i++;
+        while (i < line.length() && line.charAt(i) != ',')
+            author += line.charAt(i++);
+        i++;
+        while (i < line.length() && line.charAt(i) != ',')
+            copies += line.charAt(i++);
+        if (copies.isBlank())
+            return new Book(name, author);
+        return new Book(name, author, (int)Integer.parseInt(copies));
+    }
+    public static Book[] parseLibrary(){
+        Book[] books = new Book[MAX_BOOKS];
+        int i = 0;
+        File file = new File(FILE_NAME);
+        if (file.exists()){
+            try (Scanner in = new Scanner(file)){
+                while (in.hasNextLine())
+                    books[i++] = parseLine(in.nextLine());
+            }catch(Exception e){
+                System.out.println("An exception occured: " + e);
+            }
+            return books;
+        }else{
+            throw new RuntimeException("The file: does not exist");
+        }
+    }
+
+
+    
+}
