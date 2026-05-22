d8719062f0be63797829ccc4eaeed660df655284
Kairav <kairavdutta@gmail.com>
2026-05-22 12:08:20 +0530

    Created Fines and Issues classes with attributes

--- src/Fine.java
+++ src/Fine.java
@@ -1,14 +1,21 @@
-
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
-
-public class Fine{
+public class Fine {
     LocalDateTime time;
-    LocalDateTime dateIssued;
-    LocalDateTime returnDate;
+    Issues issued;
     double amount;
     boolean isDeducted;
-    private void calculateFine(Book book){
-        this.amount = book.getPrice() * 0.1; //per day
+    private void calculateFine(Book book) {
+        long daysLate = ChronoUnit.DAYS.between(
+                issued.dateIssued,
+                issued.returnDate
+        );
+        // 10% of book price per day
+        this.amount = book.getPrice() * 0.1 * daysLate;
+    }
+    public Fine(Issues issue){
+        this.issued = issue;
+        this.calculateFine(issue.book);
     }
 }
--- src/Issues.java
+++ src/Issues.java
@@ -1,3 +1,9 @@
+
+import java.time.LocalDateTime;
+
 public class Issues{
-    
+    LocalDateTime dateIssued;
+    LocalDateTime returnDate;
+    BaseUser issuedBy;
+    Book book;
 }
