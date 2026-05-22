b0b3635fbf17e91f40d93c4b0d7d5872534c4586
Kairav <kairavdutta@gmail.com>
2026-05-22 12:08:00 +0530

    Created logging class methods

---
+++ src/Log.java
@@ -0,0 +1,20 @@
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class Log{
+    public static void log(String message){
+        try(FileWriter myWriter = new FileWriter("filename.txt")) {
+            message = createLog(message);
+            myWriter.write(message);
+            System.out.println("Action logged successfully");
+        } catch (IOException e) {
+            System.out.println("An error occurred: " + e);
+        }
+    }
+    private static String createLog(String message){
+        message = LocalDateTime.now() + ":\n" + message + "\r\n\r\n";
+        return message;
+    }
+}
