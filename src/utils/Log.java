365bf3de7bbd93a5d49656901a54f92fb03a2280
Kairav <kairavdutta@gmail.com>
2026-05-24 15:31:17 +0530

    made log append to file instead of overwrite

--- src/utils/Log.java
+++ src/utils/Log.java
@@ -5,17 +5,18 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 
 public class Log{
+    private static final String LOG_FILE = "log.txt";
+
     public static void log(String message){
-        try(FileWriter myWriter = new FileWriter("filename.txt")) {
+        try(FileWriter myWriter = new FileWriter(LOG_FILE, true)) {
             message = createLog(message);
             myWriter.write(message);
-            System.out.println("Action logged successfully");
         } catch (IOException e) {
-            System.out.println("An error occurred: " + e);
+            System.out.println("An error occurred while logging: " + e);
         }
     }
     private static String createLog(String message){
-        message = LocalDateTime.now() + ":\r\n" + message + "\r\n\r\n";
+        message = LocalDateTime.now() + ":\n" + message + "\n\n";
         return message;
     }
 }
